60d4b65c9cfa2eb2b150400240776c68ab350b8d
PauloHncaixeta <pauloHncaixeta@gmail.com>
2018-07-01 19:20:34 -0300

    inclusao partes5

--- src/pages/home/criar-tarefa/criar-tarefa.ts
+++ src/pages/home/criar-tarefa/criar-tarefa.ts
@@ -52,33 +52,10 @@ export class CriarTarefaPage {
     
   }
 
-  addTarefaPhoto () {
-    let criaTarefaTexto = {
-      tarefa: this.CategoriaTarefa,
-      foto: this.fotoItem,
-    }
-    this.tarefaProvider.criaTarefaTexto(criaTarefaTexto).then((res) => {
-      this.navCtrl.setRoot(HomePage)
-    }).catch((err) => {
-      let alert = this.alertCtrl.create({
-        title: 'Atenção',
-        subTitle: 'Erro ao criar tarefa! ' + JSON.stringify(err),
-        buttons: 
-        [       
-          {
-            text: 'OK'
-          }
-        ]
-      })
-
-      alert.present();
-    })
-  }
-
-  salvarFotoTarefa(){
-    let options : CameraOptions = {
-      targetWidth:800,
-      targetHeight:600,
+  salvarFotoTarefa () {
+    let options: CameraOptions = {
+      targetWidth: 800,
+      targetHeight: 600,
       quality: 50,
       correctOrientation: true,
       saveToPhotoAlbum:true,
@@ -87,11 +64,14 @@ export class CriarTarefaPage {
       sourceType: this.camera.PictureSourceType.CAMERA
       
     }
-    let criaTarefaTexto = {
-      tarefa: this.CategoriaTarefa,
-      foto: this.camera.getPicture(options).then((res) => {
-      this.tarefaProvider.criaTarefaFoto(criaTarefaFoto) = "data:image/jpeg;base64," + res;
-    }).catch((err)=>{
+    this.camera.getPicture(options).then(res => {
+      let fotoBase64 = `data:image/jpeg;base64,${res}`
+      let criaTarefaTexto = {
+        tarefa: this.CategoriaTarefa,
+        foto: fotoBase64
+      }
+      this.tarefaProvider.criaTarefaFoto(criaTarefaTexto)
+    }).catch(err => {
       console.log(err)
     })
   }
